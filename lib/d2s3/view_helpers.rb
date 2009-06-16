a5a44efa5fd9db9edfb725953f30170c686e3194
Jakub <qoobaa@gmail.com>
2009-06-16 22:27:15 +0200

    removed 'signature' calculations

--- lib/d2s3/view_helpers.rb
+++ lib/d2s3/view_helpers.rb
@@ -1,50 +1,52 @@
-require 'base64'
-
 module D2s3
   module ViewHelpers
-    include D2s3::Signature
-
     def s3_upload_form_tag(options = {})
-      bucket          = D2s3.configuration.bucket
-      access_key_id   = D2s3.configuration.access_key_id
-      protocol        = options[:protocol] || "http"
-      key             = options[:key] || ''
-      redirect        = options[:redirect] || '/'
-      acl             = options[:acl] || 'public-read'
-      expiration_date = (options[:expiration_date] || 10.hours).from_now.strftime('%Y-%m-%dT%H:%M:%S.000Z')
-      max_filesize    = options[:max_filesize] || 1.megabyte
-      submit_button   = options[:submit_button] || %(<input type="submit" value="#{I18n.t("button.upload", :default => "Upload")}">)
+      bucket            = D2s3.configuration.bucket
+      access_key_id     = D2s3.configuration.access_key_id
+      secret_access_key = D2s3.configuration.secret_access_key
 
-      options[:form] ||= {}
-      options[:form][:id] ||= 'upload-form'
-      options[:form][:class] ||= 'upload-form'
+      raise "Please configure D2s3 before use" unless bucket and access_key_id and secret_access_key
 
-      policy = Base64.encode64(
-                               %({"expiration": "#{expiration_date}",
-          "conditions": [
-            {"bucket": "#{bucket}"},
-            ["starts-with", "$key", "#{key}"],
-            {"acl": "#{acl}"},
-            {"success_action_redirect": "#{redirect}"},
-            ["content-length-range", 0, #{max_filesize}]
-          ]
-        })).gsub(/\n|\r/, '')
+      protocol          = options.delete(:protocol) || "http"
+      key               = options.delete(:key) || ""
+      redirect          = options.delete(:redirect) || "/"
+      acl               = options.delete(:acl) || "public-read"
+      expiration_date   = (options.delete(:expiration_date) || 10.hours).from_now.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+      max_filesize      = options.delete(:max_filesize) || 1.megabyte
+      submit            = options.delete(:submit_button) || submit_tag(I18n.t("button.upload", :default => "Upload"))
 
-      signature = b64_hmac_sha1(D2s3.configuration.secret_access_key, policy)
-      out = ""
-      out << %(
-          <form action="#{protocol}://#{bucket}.s3.amazonaws.com/" method="post" enctype="multipart/form-data" id="#{options[:form][:id]}" class="#{options[:form][:class]}" style="#{options[:form][:style]}">
-          <input type="hidden" name="key" value="#{key}/${filename}">
-          <input type="hidden" name="AWSAccessKeyId" value="#{access_key_id}">
-          <input type="hidden" name="acl" value="#{acl}">
-          <input type="hidden" name="success_action_redirect" value="#{redirect}">
-          <input type="hidden" name="policy" value="#{policy}">
-          <input type="hidden" name="signature" value="#{signature}">
-          <input name="file" type="file">#{submit_button}
-          </form>
-        )
+      policy_document = <<-eos
+        {
+          "expiration": "#{expiration_date}",
+          "conditions":
+            [
+              {"bucket": "#{bucket}"},
+              ["starts-with", "$key", "#{key}"],
+              {"acl": "#{acl}"},
+              {"success_action_redirect": "#{redirect}"},
+              ["content-length-range", 0, #{max_filesize}]
+            ]
+        }
+      eos
+
+      policy = Base64.encode64(policy_document).gsub("\n","")
+      signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new("sha1"), secret_access_key, policy)).gsub("\n","")
+
+      options[:action] ||= "#{protocol}://#{bucket}.s3.amazonaws.com/"
+      options[:method] ||= "post"
+      options[:enctype] ||= "multipart/form-data"
+
+      content_tag :form, options do
+        content = ""
+        content << hidden_field_tag("key", "#{key}/${filename}")
+        content << hidden_field_tag("AWSAccessKeyId", access_key_id)
+        content << hidden_field_tag("acl", acl)
+        content << hidden_field_tag("success_action_redirect", redirect)
+        content << hidden_field_tag("policy", policy)
+        content << hidden_field_tag("signature", signature)
+        content << file_field_tag("file")
+        content << submit
+      end
     end
   end
 end
-
-ActionView::Base.send(:include, D2s3::ViewHelpers) if defined?(ActionView::Base)
